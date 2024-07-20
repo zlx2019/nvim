448e2c8f6ff4a34029668c5109044e079568a5b0
Zero <15133024857@163.com>
2024-07-20 18:47:34 +0800

    futere: init

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+-- Copilot 配置
+
+if true then return end
+
+---@type LazySpec
+return {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "BufEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 150,
+        keymap = {
+          accept = "<C-;>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+    },
+  }
---
+++ lua/plugins/notify.lua
@@ -0,0 +1,10 @@
+-- 消息通知窗口设置
+
+---@type LazySpec
+return {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 5000,
+      background_colour = "#000000",
+    },
+  }
